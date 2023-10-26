c69b79e3c409f1e77951e798fe1f0f91e94b2659
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-10-26 18:07:43 +0200

    feat: Initial Frontend

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-
+import Navigation from "./components/Navigation.vue";
 </script>
 
 <template>
-  
+  <VApp>
+    <Navigation />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <VFooter>2023 CryptoScam</VFooter>
+  </VApp>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {ref} from 'vue'; 
+
+const navigation = ref(false);
+</script>
+
+<template id="Navigation">
+    <VAppBar rounded>
+          <VAppBarNavIcon @click.stop="navigation = !navigation"/>
+          <VAppBarTitle>CryptoScam</VAppBarTitle>
+          <VSpacer/>
+          <VIcon icon="$vuetify"></VIcon>
+    </VAppBar>
+
+    <VNavigationDrawer v-model="navigation" location="left" color="background">
+        <VList>
+            <VListItem title="Home" to="/"></VListItem>
+            <VListItem title="Past Proposals" to="/PastProposals"></VListItem>
+            <VListItem title="Current Proposals" to="/CurrentProposals"></VListItem>
+            <VListItem title="Create New Proposal" to="/NewProposal"></VListItem>
+        </VList>
+    </VNavigationDrawer>
+</template>
+
+<style scoped></style>
