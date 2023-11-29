3e6eb2b1e1190cf116874d84c313972565dd7b0e
kaj.habegger <kaj.habegger@ost.ch>
2023-11-29 22:55:15 +0100

    Update loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="text-center">
+        <v-progress-circular
+            indeterminate
+            color="primary"
+            ></v-progress-circular>
+    </div>
+</template>
